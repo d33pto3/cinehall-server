320b06c2e19e0f748c5958965c745e4fcf6a47f2
rakinodcl <rakinodcl@gmail.com>
2025-07-03 09:10:59 +0600

    ssl-commerz add

--- src/controllers/ticket.controller.ts
+++ src/controllers/ticket.controller.ts
@@ -5,17 +5,18 @@ import AppError from "../utils/AppError";
 
 const ObjectId = mongoose.Types.ObjectId;
 
-export const createTicket = async (req: Request, res: Response) => {
+export const createTicketsForBooking = async (req: Request, res: Response) => {
   const { bookingId, userId, showId, seatId } = req.body;
 
   const booking = await Booking.findById(bookingId);
-  const user = await User.findById(userId);
-  const show = await Show.findById(showId);
-  const seat = await Seat.findById(seatId);
 
   if (!ObjectId.isValid(bookingId) || !booking) {
     throw new AppError("Provide valid Booking!", 400);
   }
+
+  const user = await User.findById(userId);
+  const show = await Show.findById(showId);
+  const seat = await Seat.findById(seatId);
 
   if (!ObjectId.isValid(userId) || !user) {
     throw new AppError("Provide valid User!", 400);
