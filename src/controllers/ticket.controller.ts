cd4d96c769ad074c007089a1a8c86708dc3b2f0a
deeptoe <rakinfrhn@gmail.com>
2025-07-06 14:17:49 +0600

    feat: get ticket

--- src/controllers/ticket.controller.ts
+++ src/controllers/ticket.controller.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 // import { Booking, Seat, Show, User } from "../models";
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 // import AppError from "../utils/AppError";
 import { Ticket } from "../models/ticket.model";
+import AppError from "../utils/AppError";
+const ObjectId = mongoose.Types.ObjectId;
 
 // const ObjectId = mongoose.Types.ObjectId;
 
@@ -41,5 +43,23 @@ export const getAllTickets = async (_req: Request, res: Response) => {
     success: true,
     message: "Successfully fetched all tickets!",
     data: tickets,
+  });
+};
+
+export const getTicket = async (req: Request, res: Response) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    throw new AppError("Ticket ID is not valid", 400);
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    throw new AppError("Ticket not found", 404);
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Successfully fetched ticket!",
+    data: ticket,
   });
 };
--- src/routes/ticket.route.ts
+++ src/routes/ticket.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { getAllTickets } from "../controllers/ticket.controller";
+import { getAllTickets, getTicket } from "../controllers/ticket.controller";
 import asyncHandler from "../middlewares/asyncHandler";
 
 const router = express.Router();
 
 router.route("/").get(asyncHandler(getAllTickets));
+router.route("/:id").get(asyncHandler(getTicket));
 
 export default router;
